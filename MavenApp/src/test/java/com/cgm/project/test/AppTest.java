8b039db553451bea3db98f7ae5898a1716cc934a
Ajay-1998 <Ajay@exam.com>
2020-09-30 12:50:41 +0530

    Test Done

---
+++ MavenApp/src/test/java/com/cgm/project/test/AppTest.java
@@ -0,0 +1,29 @@
+package com.cgm.project.test;
+
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test1() {
+		System.out.println("Test1");
+	}
+
+	@Test
+	public void test2() {
+		System.out.println("Test2");
+	}
+
+	@Test
+	public void test3() {
+		//fail();
+		System.out.println("Test3");
+	}
+
+	@Test
+	public void test4() {
+		//fail();
+		System.out.println("Test4");
+	}
+}
